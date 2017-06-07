28992a403f462ffe156f60337dd14dc5a98a611f
Jerome Zago <jerome.zago@gmail.com>
2017-06-07 11:38:54 +0200

    Personalizing the user profile page

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -6,6 +6,35 @@
  */
 
 module.exports = {
+
+  showProfilePage: function (req, res) {
+
+    if (!req.session.userId) {
+      return res.redirect('/');
+    }
+
+    User.findOne(req.session.userId, function (err, user){
+      if (err) {
+        console.log('error: ', err);
+        return res.negotiate(err);
+      }
+
+      if (!user) {
+        sails.log.verbose('Session refers to a user who no longer exists- did you delete a user, then try to refresh the page with an open tab logged-in as that user?');
+        return res.view('homepage');
+      }
+
+      return res.view('profile', {
+        me: {
+          id: user.id,
+          email: user.email,
+          gravatarURL: user.gravatarURL,
+          admin: user.admin
+        }
+      });
+    });
+  },
+
   showHomePage: function (req, res) {
 
     if (!req.session.userId) {
--- config/routes.js
+++ config/routes.js
@@ -14,18 +14,8 @@ module.exports.routes = {
   'GET /': 'PageController.showHomePage',
   'GET /videos': 'PageController.showVideosPage',
   'GET /administration': 'PageController.showAdminPage',
+  'GET /profile': 'PageController.showProfilePage',
 
-  'GET /profile': {
-    view: 'profile',
-    locals: {
-      me: {
-        id: 1,
-        gravatarURL: 'http://www.gravatar.com/avatar/ef3eac6c71fdf24b13db12d8ff8d1264?',
-        email: 'sailsinaction@gmail.com',
-        username: 'sails-in-action'
-      }
-    }
-  },
   'GET /edit-profile': {
     view: 'edit-profile',
     locals: {
